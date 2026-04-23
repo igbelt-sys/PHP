0f4245f460f3a580d6fb4b454b6c859c37f73b9b
igor beltramo <beltramoigor1@gmail.com>
2026-04-23 17:11:42 -0300

    chore: atualiza atividades de PHP

---
+++ Atividades/S2_R2_AT1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Codigo em html</h1>
+    <?php 
+        echo "codigo em php";
+        //comentario dentro do php com /
+
+        echo "<strong> usando tags <strong> ";
+    </php>
+</body>
+</html>
